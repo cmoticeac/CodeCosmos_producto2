f37e344dfb244ae84c3c76407dd35edc24d3aa36
cmoticeac <147082946+cmoticeac@users.noreply.github.com>
2024-11-04 11:53:51 +0100

    ActualizadoListaJugadoresIniciales

--- src/app/detail-component/detail-component.component.ts
+++ src/app/detail-component/detail-component.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-component',
   standalone: true, // Agregar esta línea para que sea standalone
+  imports: [CommonModule],
   templateUrl: './detail-component.component.html',
   styleUrls: ['./detail-component.component.css'],
 })
