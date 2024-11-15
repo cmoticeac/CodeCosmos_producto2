894dce16bc89d9b261c4f5725b0e25218aa6d290
Liliana <ldiazib@uoc.edu>
2024-11-15 22:24:40 +0100

    Adicionados el resto de controles en el detalle, incluyendo video e imagenes

--- src/app/detail-component/detail-component.component.ts
+++ src/app/detail-component/detail-component.component.ts
@@ -13,6 +13,16 @@ import { FormsModule } from '@angular/forms'; // Importa FormsModule
   styleUrls: ['./detail-component.component.css'],
 })
 export class DetailComponent {
+positions: any[] = [ "Base", "Alero", "Defensa", "Poste" ];
+images: any[] = [
+  {"link": "assets/imagenes/player01_01.png", "name": "Player 01"},
+  {"link": "assets/imagenes/player02_01.png", "name": "Player 02"},
+  {"link": "assets/imagenes/player03_01.png", "name": "Player 03"},
+  {"link": "assets/imagenes/player04_01.png", "name": "Player 04"},
+  {"link": "assets/imagenes/player05_01.png", "name": "Player 05"},
+  {"link": "assets/imagenes/player06_01.png", "name": "Player 06"}
+
+];
   toggleEdit() {
     this.readonly = !this.readonly;
   }
@@ -21,6 +31,16 @@ export class DetailComponent {
   @Output() closeDetailEvent = new EventEmitter<void>();
   @Output() savePlayerEvent = new EventEmitter<any>();
   readonly: boolean = true;
+  videos: any[] = [];
+
+  ngOnInit(): void {
+    this.videos.push({ "link": "assets/videos/player01.mp4", "name": "Player 01"});
+    this.videos.push({ "link": "assets/videos/player02.mp4", "name": "Player 02"});
+    this.videos.push({ "link": "assets/videos/player03.mp4", "name": "Player 03"});
+    this.videos.push({ "link": "assets/videos/player04.mp4", "name": "Player 04"});
+    this.videos.push({ "link": "assets/videos/player05.mp4", "name": "Player 05"});
+    this.videos.push({ "link": "assets/videos/player06.mp4", "name": "Player 06"});
+  }
 
   closeDetail() {
     this.closeDetailEvent.emit();
